83dbacfbb07380b2e1e26ecba6f4eb1a1c432365
Breno Mendes <89087720+s2breninn@users.noreply.github.com>
2024-02-16 11:30:43 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/template/NavBar"
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <p className="text-center bg-red-600">oii</p>
+    </>
+  )
+}
+
+export default App
